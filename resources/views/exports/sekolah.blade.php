e26e85ae7efb1f72779625f92fcd44bc5e8db34c
masadi <masadi.com@gmail.com>
2021-04-25 02:00:25 +0700

    Update sekolah.blade.php

--- resources/views/exports/sekolah.blade.php
+++ resources/views/exports/sekolah.blade.php
@@ -38,6 +38,12 @@
         <td>6</td>
         <td>7</td>
         <td>8</td>
+        <td>9</td>
+        <td>10</td>
+        <td>11</td>
+        <td>12</td>
+        <td>13</td>
+        <td>14</td>
     </tr>
     @endforeach
     <tr>
